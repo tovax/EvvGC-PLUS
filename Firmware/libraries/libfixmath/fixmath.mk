69d6db2e2c6d80be24d3e37c58f21d633187a2a4
SVentas <SVentas@users.noreply.github.com>
2015-03-20 19:45:42 +0200

    Fix16 libraries added.

---
+++ Firmware/libraries/libfixmath/fixmath.mk
@@ -0,0 +1,8 @@
+# List of all the board related files.
+FIXMATHSRC = ${SRCDIR}/libraries/libfixmath/fix16.c \
+             ${SRCDIR}/libraries/libfixmath/fix16_exp.c \
+             ${SRCDIR}/libraries/libfixmath/fix16_sqrt.c \
+             ${SRCDIR}/libraries/libfixmath/fix16_trig.c
+
+# Required include directories
+FIXMATHINC = ${SRCDIR}/libraries/libfixmath
---
+++ Firmware/libraries/libfixmatrix/fixmatrix.mk
@@ -0,0 +1,8 @@
+# List of all the board related files.
+FIXMATRIXSRC = ${SRCDIR}/libraries/libfixmatrix/fixarray.c \
+               ${SRCDIR}/libraries/libfixmatrix/fixmatrix.c \
+               ${SRCDIR}/libraries/libfixmatrix/fixquat.c \
+               ${SRCDIR}/libraries/libfixmatrix/fixvector3d.c
+
+# Required include directories
+FIXMATRIXINC = ${SRCDIR}/libraries/libfixmatrix
