91bf23decfa19991a49749353e548ded741c742e
SVentas <SVentas@users.noreply.github.com>
2015-04-27 18:46:36 +0300

    Update to ChibiOS 2.6.8

---
+++ Firmware/chibios_2.6.8/hal/hal.mk
@@ -0,0 +1,25 @@
+# List of all the ChibiOS/RT HAL files, there is no need to remove the files
+# from this list, you can disable parts of the HAL by editing halconf.h.
+HALSRC = ${SRCDIR}/chibios_2.6.8/hal/src/hal.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/adc.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/can.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/ext.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/gpt.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/i2c.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/icu.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/mac.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/mmc_spi.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/mmcsd.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/pal.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/pwm.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/rtc.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/sdc.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/serial.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/serial_usb.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/spi.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/tm.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/uart.c \
+         ${SRCDIR}/chibios_2.6.8/hal/src/usb.c
+
+# Required include directories
+HALINC = ${SRCDIR}/chibios_2.6.8/hal/include
